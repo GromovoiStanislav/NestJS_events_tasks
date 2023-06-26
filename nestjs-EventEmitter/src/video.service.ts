0d4a3a8cd8d71b98d3e6b25eadbe27fa287ff9e5
Gromovoi Stanislav <gromila12@rambler.ru>
2023-06-26 10:24:37 +0600

    nestjs-EventEmitter

--- nestjs-EventEmitter/src/video.service.ts
+++ nestjs-EventEmitter/src/video.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { EventEmitter2 } from "@nestjs/event-emitter";
 import { VideoCreatedEvent } from "./video-created.event";
+import { log } from "util";
 
 @Injectable()
 export class VideoService {
@@ -14,32 +15,104 @@ export class VideoService {
     const title = "How to smash like button";
     console.log("Publishing new video");
 
+
+
     // const result = await this.eventEmitter.emitAsync("video.created", new VideoCreatedEvent(title));
-    // console.log(result);
+    // console.log("result",result);
     // Вывод:
     // Publishing new video
     // 1. Notifying... How to smash like button
     // 2. handling in doSomething... How to smash like button
-    // [ 1, 2 ]
+    // result [ 1, 2 ]
     // All done!
 
+    // если была Error():
+    // Publishing new video
+    // ERROR [ExceptionsHandler] Error
+    // {
+    //   "statusCode": 500,
+    //   "message": "Internal server error"
+    // }
 
 
 
-    //this.eventEmitter.emit("video.created", new VideoCreatedEvent(title));
+    const result = await this.eventEmitter.emitAsync("video.created1", new VideoCreatedEvent(title));
+    console.log("result",result);
     // Вывод:
     // Publishing new video
     // 1. Notifying... How to smash like button
     // 2. handling in doSomething... How to smash like button
+    // result [ 1, 2 ]
     // All done!
 
+    // если была Error():
+    // Publishing new video
+    // 2. handling in doSomething... How to smash like button
+    // ERROR [ExceptionsHandler] Error
+    // {
+    //   "statusCode": 500,
+    //   "message": "Internal server error"
+    // }
 
-    this.eventEmitter.emit("video.created2", new VideoCreatedEvent(title));
+
+
+
+
+    // const result = this.eventEmitter.emit("video.created", new VideoCreatedEvent(title));
+    // console.log("result",result);
     // Вывод:
     // Publishing new video
+    // 1. Notifying... How to smash like button
+    // 2. handling in doSomething... How to smash like button
+    // result true
+    // All done!
+
+    // если была Error():
+    // Publishing new video
+    // ERROR [ExceptionsHandler] Error
+    // {
+    //   "statusCode": 500,
+    //   "message": "Internal server error"
+    // }
+
+
+
+    // const result = this.eventEmitter.emit("video.created2", new VideoCreatedEvent(title));
+    // console.log("result",result);
+    // Вывод:
+    // Publishing new video
+    // result true
     // All done!
     // 1. Notifying... How to smash like button
     // 2. handling in doSomething... How to smash like button
+
+    // если была Error():
+    // Publishing new video
+    // result true
+    // All done!
+    // throw new Error("Error")
+
+
+
+    // const result = this.eventEmitter.emit("video.created3", new VideoCreatedEvent(title));
+    // console.log("result",result);
+    // Вывод:
+    // Publishing new video
+    // throw new Error()
+    // 1. Notifying... How to smash like button
+    // 2. handling in doSomething... How to smash like button
+    //result true
+    // All done!
+
+    // если была Error():
+    // Publishing new video
+    // 2. handling in doSomething... How to smash like button
+    // result true
+    // All done!
+    // throw new Error("Error")
+
+
+
 
 
     console.log("All done!");
--- nestjs-EventEmitter/src/viewer.service.ts
+++ nestjs-EventEmitter/src/viewer.service.ts
@@ -8,10 +8,10 @@ export class ViewerService {
 
   @OnEvent("video.created")
   notify({ title }: VideoCreatedEvent) {
+    //throw new Error("Error")
     console.log("1. Notifying...", title);
     return 1;
   }
-
 
   @OnEvent("video.created")
   doSomething({ title }: VideoCreatedEvent) {
@@ -20,16 +20,45 @@ export class ViewerService {
   }
 
 
-  @OnEvent("video.created2", { async: true })
-  notify2({ title }: VideoCreatedEvent) {
+  @OnEvent("video.created1")
+  async notify1({ title }: VideoCreatedEvent) {
+    //throw new Error("Error")
     console.log("1. Notifying...", title);
+    return 1;
   }
 
+  @OnEvent("video.created1")
+  async doSomething1({ title }: VideoCreatedEvent) {
+    console.log("2. handling in doSomething...", title);
+    return 2;
+  }
+
+
+  @OnEvent("video.created2", { async: true })
+  notify2({ title }: VideoCreatedEvent) {
+    //throw new Error("Error")
+    console.log("1. Notifying...", title);
+    return 1;
+  }
 
   @OnEvent("video.created2", { async: true })
   doSomething2({ title }: VideoCreatedEvent) {
     console.log("2. handling in doSomething...", title);
+    return 2;
   }
 
+
+  @OnEvent("video.created3")
+  async notify3({ title }: VideoCreatedEvent) {
+    //throw new Error("Error")
+    console.log("1. Notifying...", title);
+    return 1;
+  }
+
+  @OnEvent("video.created3")
+  async doSomething3({ title }: VideoCreatedEvent) {
+    console.log("2. handling in doSomething...", title);
+    return 2;
+  }
 
 }
